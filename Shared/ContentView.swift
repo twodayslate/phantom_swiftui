86548cff91951fe3d5798d8eb896f5f8264bb77b
twodayslate <zac@gorak.us>
2021-08-02 22:42:03 -0400

    beautiful animation for the header

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -18,18 +18,20 @@ struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
     @State var scrollOffset: CGFloat = 0
-    @State var headerOffset: CGFloat = 0
+    @State var headerSize: CGSize = .zero
+    @State var priceSize: CGSize = .zero
     
     var body: some View {
             ZStack {
                 ScrollView {
                     Color.clear.frame(height:0).overlay(GeometryReader { proxy -> Color in
                         let minY = proxy.frame(in: .global).minY
-                        print("header", proxy.frame(in: .global), proxy.frame(in: .local))
+                        //print("header", proxy.frame(in: .global), proxy.frame(in: .local))
+                        print(minY, self.headerPercentage)
                         DispatchQueue.main.async {
-                            withAnimation {
+                            //withAnimation {
                                 self.scrollOffset = minY
-                            }
+                            //}
                         }
                         return Color.clear
                     }.frame(height: 0))
@@ -47,24 +49,59 @@ struct ContentView: View {
                             GeometryReader { proxy in
                                 Color.clear.padding(.top, proxy.safeAreaInsets.top)
                             }.padding(.top)
-                            VStack {
-                                HStack {
-                                    Text("Wallet 1").font(.headline).bold().foregroundColor(.accentColor).padding(.horizontal)
-                                    if self.scrollOffset < 0 {
+                            VStack(alignment: .leading) {
+                                ZStack {
+                                    HStack(alignment: .center) {
+                                        //GeometryReader { proxy in
                                         Spacer()
+                                        
+                                        Text("Wallet 1").font(.headline).bold().foregroundColor(.accentColor).padding(.horizontal).background(GeometryReader { proxy -> Color in
+                                            
+                                            DispatchQueue.main.async {
+                                                withAnimation {
+                                                    headerSize = proxy.size
+                                                }
+                                            }
+                                            
+                                            return .clear
+                                        })
+                                        .offset(x: self.scrollOffset >= 0 ? 0 : -(UIScreen.main.bounds.midX - self.headerSize.width/2) * self.headerPercentage)
+                                        .offset(y: headerSize.height/3 * self.headerPercentage)
+                                        
+                                        Spacer()
+                                    }
+                                    HStack(alignment: .center) {
+                                        //GeometryReader { proxy in
+                                        
+                                        Spacer()
+                                            
                                         Text("$17,382.82").transition(AnyTransition.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .trailing))).padding(.horizontal)
+                                            .offset(x: self.scrollOffset >= 0 ? 100 : 100 * (1.0-self.headerPercentage))
+                                            .offset(y: headerSize.height/3 * self.headerPercentage)
+                                        
+                                        
                                     }
                                 }
                                 
-                                //if self.scrollOffset >= 0 {
-                                Text("1fRFgFkx7-Y9EKu7W9efmLalK_XFXqV10s5wTlZIp60").foregroundColor(.gray).font(.caption2).padding(.bottom, 2.0).transition(AnyTransition.asymmetric(insertion: .move(edge: .bottom), removal: .move(edge: .bottom)).combined(with: .opacity)).opacity(self.scrollOffset < 0 ? 0.0 : 1.0
-                                )
-                                //}
+                                
+                                HStack(alignment: .center) {
+                                    Spacer()
+                                    //if self.scrollOffset >= 0 {
+                                    Text("1fRFgFkx7-Y9EKu7W9efmLalK_XFXqV10s5wTlZIp60").foregroundColor(.gray).font(.caption2)
+                                        .padding(.bottom, 4.0)
+                                        .transition(AnyTransition.asymmetric(insertion: .move(edge: .bottom), removal: .move(edge: .bottom)).combined(with: .opacity))
+                                        .opacity(self.scrollOffset >= 0 ? 1.0 : 1.0-Double(self.headerPercentage))
+                                    //}
+                                    Spacer()
+                                }
+                                
                             }.padding(.top, 100)
                                 //.padding(.top, proxy.safeAreaInsets.top).padding(.top).frame(height: nil)
 //                            }}
                         Divider()
-                        }.background(Color(UIColor.secondarySystemBackground)).offset(y:-self.scrollOffset - 100)
+                        }
+                        .background(Color(UIColor.secondarySystemBackground))
+                        .offset(y:-self.scrollOffset - 100)
                     .zIndex(1.0)
                     
                     VStack {
@@ -75,7 +112,18 @@ struct ContentView: View {
                             Text("+13.3%").font(.title3).foregroundColor(.green).padding(4.0).background(Color.green.opacity(0.2)).cornerRadius(8.0)
                             Spacer()
                         }
-                    }.padding().opacity(self.scrollOffset < -40 ? 0.0 : 1.0).offset(y:-100).zIndex(0.0)
+                    }.padding()
+                    .background(GeometryReader { proxy -> Color in
+                        
+                        DispatchQueue.main.async {
+                            withAnimation {
+                                priceSize = proxy.size
+                            }
+                        }
+                        
+                        return .clear
+                    })
+                    .opacity(self.scrollOffset >= 0 ? 1.0 : 1.0-Double(self.headerPercentage)).offset(y:-100).zIndex(0.0)
                     
                     ForEach(0..<2) { _ in
                         HStack(alignment: .center) {
@@ -124,6 +172,14 @@ struct ContentView: View {
             }
         }
     
+    
+    var headerPercentage: CGFloat {
+        if self.scrollOffset > 0 {
+            return 0
+        }
+        
+        return min(1.0, (abs(self.scrollOffset) / priceSize.height))
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
